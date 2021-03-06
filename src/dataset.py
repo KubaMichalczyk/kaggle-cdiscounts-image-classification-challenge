c2328cc77ccb0df964bdf8bcd8f135dae3b6a6aa
KubaMichalczyk <kuba.michalczyk@gmail.com>
2021-03-06 15:21:35 +0100

    Implemented CDiscountDataset class

--- src/dataset.py
+++ src/dataset.py
@@ -1,17 +1,113 @@
-# This scripts will contain Dataset class implementation
+import struct
+import bson
+import pandas as pd
+import numpy as np
+import cv2
+import torch
 
-class ClassificationDataset:
+from tqdm import tqdm
+
+class CDiscountDataset:
     
-    def __init__(self, image_paths, targets, resize=None, augmentations=None):
-        self.image_paths = image_paths
-        self.targets = targets
+    def __init__(self, input_path, items=None, metadata_file=None, image_id=0, random=False, resize=None, augmentations=None):
+        
+        self.input_path = input_path
+        self.items = items
         self.resize = resize
         self.augmentations = augmentations
+        self.metadata = None
+        self.image_id = image_id
+        self.random = random
+        
+        if metadata_file is not None:
+            self.metadata = pd.read_csv(metadata_file)
+        else:
+            plt.imshow(f"Dataset metadata is being extracted from {self.input_path}, this may take several minutes.")
+            self.extract_metadata()
+        
+        if items is None:
+            self.items = self.metadata.index
 
-    
+
+    def extract_metadata(self, num_items=None):
+        """Source: https://www.kaggle.com/vfdev5/random-item-access"""
+        with open(self.input_path, 'rb') as f, tqdm(num_items) as progress_bar: 
+            items = []
+            offset = 0
+            while True:        
+                progress_bar.update()
+                f.seek(offset)
+                
+                item_length_bytes = f.read(4) 
+                if len(item_length_bytes) == 0:
+                    break                
+                # Decode item length:
+                length = struct.unpack("<i", item_length_bytes)[0]
+                
+                f.seek(offset)
+                item_data = f.read(length)
+                assert len(item_data) == length, "%i vs %i" % (len(item_data), length)
+                
+                # Check if we can decode
+                item = bson.BSON.decode(item_data)
+                items.append({
+                    'item_id': item['_id'],
+                    'category_id': item['category_id'],
+                    'n_images': len(item['imgs']),
+                    'offset': offset,
+                    'length': length,
+                })
+                offset += length
+        progress_bar.close()
+        self.metadata = pd.DataFrame(items).set_index('item_id')
+
+
+    def save_metadata(self, path):
+        self.metadata.to_csv(path, index=False)
+
+
     def __len__(self):
-        return len(self.image_paths)
+        """
+        Returns the total number of items in the dataset.
+        """
+        return len(self.items)
+    
+
+    def __getitem__(self, item_id):
+        assert item_id in self.metadata.index 
+        assert self.image_id <= self.metadata.loc[item_id, 'n_images']
+        with open(self.input_path, 'rb') as f:
+            offset, length = self.metadata.loc[item_id, ['offset', 'length']]
+            f.seek(offset)
+            item_bytes = f.read(length)
+        item_decoded = bson.BSON.decode(item_bytes)
+        
+        if self.random:
+            selected = np.random.choice(self.metadata.loc[item_id, 'n_images'])
+        else:
+            selected = self.image_id
+        
+        image_bytes = item_decoded['imgs'][selected]['picture']
+        
+        image = cv2.imdecode(np.frombuffer(image_bytes, dtype=np.uint8), 
+                            cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
+        
+        if self.resize is not None:
+            image = cv2.resize(image, dim=self.resize)
+        
+        if self.augmentations is not None:
+            augmented = self.augmentations(image=image)
+            image = augmented['image']
+
+        # Torch expects channels first
+        image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+
+        target = item_decoded['category_id']
+        
+        return (
+            torch.tensor(image, dtype=torch.float),
+            torch.tensor(target, dtype=torch.long),
+        )
 
 
-    def __getitem__(self):
-        pass
